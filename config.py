282ff9ad7ebd6119cab70093430d8d4988ba8fe1
Denis Koshelev <truemacintoshchannel@gmail.com>
2023-10-08 16:15:32 -0400

    Adjust config to properly read .env file

--- config.py
+++ config.py
@@ -1,6 +1,16 @@
 class Config:
     def __init__(self) -> None:
-        self.open_ai_key = ""
+        self.open_ai_key = self.load_env_variable("OPENAPI_KEY")
+
+    def load_env_variable(self, key: str) -> str:
+        try:
+            with open('.env', 'r') as file:
+                for line in file.readlines():
+                    if line.startswith(key):
+                        return line.split('=')[1].strip()
+        except FileNotFoundError:
+            print(f"Warning: .env file not found. {key} will be set to an empty string.")
+        return ""
 
     def get_open_ai_key(self):
         return self.open_ai_key
