5e95c736e62a6332e8ec36e158fa42e3ffb925a2
Denis Koshelev <truemacintoshchannel@gmail.com>
2023-11-25 13:25:55 -0500

    Add semantic comparison and post-processing

--- datastructure/history_manager.py
+++ datastructure/history_manager.py
@@ -1,3 +1,6 @@
+from openai import OpenAI
+from config import Config
+
 class QueryHistory:
     def __init__(self):
         self._history = []
@@ -13,5 +16,44 @@ class QueryHistory:
 
     def get_history(self):
         return self._history
+    
+    def is_query_semantically_similar(self, new_query):
+        config = Config()
+        history_str = "\n".join([f"Index: {index}\nQuery: {entry['query']}\nResponse: {entry['response']}\nCommand: {entry['command']}" 
+                                for index, entry in enumerate(self._history)])
+        prompt = f"You will be provided with different queries, corresponding responsed and commands. You have to determine whether the following query is semantically similar to any of the above queries? Say just 'No', if there are no similar. Say 'Yes, X' if there is one. X should be index of the query (remember, in python indices start from 0). \n\nQuery: {new_query}\n"
 
+        client = OpenAI(
+            api_key=config.get_open_ai_key(),
+        )
+
+        response = client.chat.completions.create(
+            model="gpt-4",
+            messages=[
+                {
+                "role": "system",
+                "content": prompt
+                },
+                {
+                    "role": "user",
+                    "content": history_str 
+                }
+            ],
+            max_tokens=256,
+            n=1,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0
+        )
+
+        # print(history_str)
+        # print("openai response: ", response)
+        result = response.choices[0].message.content
+
+        if result.lower().startswith("yes"):
+            query = self._history[int(result.split(",")[1].strip())]
+            return query
+        else:
+            return False
+            
 global_query_history = QueryHistory()
